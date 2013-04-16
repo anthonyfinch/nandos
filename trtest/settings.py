8f97b61d5f1e879da12c3e2dc210b006a3091acd
Anthony Finch <anthony@trialreach.com>
2013-04-16 17:52:45 +0100

    Trying again

--- trtest/settings.py
+++ trtest/settings.py
@@ -23,9 +23,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nandos',                      # Or path to database file if using sqlite3.
-        'USER': 'nandos',                      # Not used with sqlite3.
-        'PASSWORD': 'nandos',                  # Not used with sqlite3.
+        'NAME': 'circle_test',                      # Or path to database file if using sqlite3.
+        'USER': 'Ubuntu',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
