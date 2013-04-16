6f7fbd6721e93e7e368db58c3361ca1b268680d4
Anthony Finch <anthony@trialreach.com>
2013-04-16 17:54:30 +0100

    Fixed typo

--- trtest/settings.py
+++ trtest/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'circle_test',                      # Or path to database file if using sqlite3.
-        'USER': 'Ubuntu',                      # Not used with sqlite3.
+        'USER': 'ubuntu',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
