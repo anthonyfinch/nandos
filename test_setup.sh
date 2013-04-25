dccc46759c17e03e1bf8b0673cd10aa60bf8dd41
Anthony Finch <actionspeakslouder@gmail.com>
2013-04-25 17:10:53 +0100

    Silly error in setup

--- test_setup.sh
+++ test_setup.sh
@@ -6,7 +6,6 @@ echo $TDDIUM_DB_NAME
 
 #createdb "$TDDIUM_DB_NAME"
 createdb template_postgis
-createdb "$TDDIUM_DB_NAME"
 
 psql template_postgis -c 'CREATE EXTENSION postgis;'
 psql "$TDDIUM_DB_NAME" -c 'CREATE EXTENSION postgis;'
