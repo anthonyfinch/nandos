41b6334a31d6fed93a1919585c3d2c6b737b9f3f
Anthony Finch <actionspeakslouder@gmail.com>
2013-04-25 17:24:13 +0100

    BLARGH

--- test_setup.sh
+++ test_setup.sh
@@ -6,6 +6,8 @@ echo $TDDIUM_DB_NAME
 
 #createdb "$TDDIUM_DB_NAME"
 createdb template_postgis
+createdb "test_$TDDIUM_DB_NAME"
 
 psql template_postgis -c 'CREATE EXTENSION postgis;'
 psql "$TDDIUM_DB_NAME" -c 'CREATE EXTENSION postgis;'
+psql "test_$TDDIUM_DB_NAME" -c 'CREATE EXTENSION postgis;'
