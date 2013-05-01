2630d8d12e470427057fb2520d9d045c11c60580
Anthony Finch <actionspeakslouder@gmail.com>
2013-05-01 14:05:25 +0100

    Updates settings for drone

--- trtest/settings.py
+++ trtest/settings.py
@@ -23,9 +23,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': get_env_variable('TDDIUM_DB_NAME'),                      # Or path to database file if using sqlite3.
-        'USER': get_env_variable('TDDIUM_DB_USER'),                      # Not used with sqlite3.
-        'PASSWORD': get_env_variable('TDDIUM_DB_PASSWORD'),                  # Not used with sqlite3.
+        'NAME': 'nandos',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
