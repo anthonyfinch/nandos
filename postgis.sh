90577eab856a03a5a4a0fd81eca3b10af5bdb0b4
Anthony Finch <actionspeakslouder@gmail.com>
2013-05-03 15:08:44 +0100

    HAHA

---
+++ postgis.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+POSTGIS_SQL_PATH=/usr/share/postgresql/9.1/contrib/postgis-1.5
+
+createdb -E UTF8 template_postgis && \
+( createlang -d template_postgis -l | grep plpgsql || createlang -d template_postgis plpgsql ) && \
+psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';" && \
+psql -d template_postgis -f $POSTGIS_SQL_PATH/postgis.sql && \
+psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql && \
+psql -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" && \
+psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
